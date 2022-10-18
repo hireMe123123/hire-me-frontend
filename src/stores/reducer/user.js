d475a3e223b42d6083e3a33c40126f74c6b9be4c
mabdullah12101 <muhabdullah12101@gmail.com>
2022-10-18 16:48:09 +0700

    first commit

---
+++ src/stores/reducer/user.js
@@ -0,0 +1,16 @@
+const initialState = {
+  data: {},
+  isLoading: false,
+  isError: false,
+  message: "",
+};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+export default user;
