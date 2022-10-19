bf6264431eb37130c439902eb899b1b1f8b82efa
azkar-sh <akbar.dizir@gmail.com>
2022-10-19 17:07:13 +0700

    task: list portofolio and experience

--- src/components/ListPortofolio/index.jsx
+++ src/components/ListPortofolio/index.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import noPhoto from "../../assets/img/no-photo.png";
 
 export default function ListPortofolio() {
   return (
     <div>
-      <div className="fw-bold h5">Portofolioo</div>
+      <div className="d-flex flex-row justify-content-evenly gap-5">
+        <div className="col ">
+          <img src={noPhoto} alt="" className="w-100 rounded-3" />
+          <div className="h6 text-center mt-2">Portofolio App</div>
+        </div>
+        <div className="col ">
+          <img src={noPhoto} alt="" className="w-100 rounded-3" />
+          <div className="h6 text-center mt-2">Portofolio App</div>
+        </div>
+        <div className="col ">
+          <img src={noPhoto} alt="" className="w-100 rounded-3" />
+          <div className="h6 text-center mt-2">Portofolio App</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/UserProfile/index.jsx
+++ src/pages/UserProfile/index.jsx
@@ -1,91 +1,100 @@
 import React from "react";
-import noPhoto from "../../assets/images/no-photo.png";
+import noPhoto from "../../assets/img/no-photo.png";
 import ListPortofolio from "../../components/ListPortofolio";
 import ListPengalaman from "../../components/ListPengalaman";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import "./index.css";
 
 export default function UserProfile() {
   return (
-    <div className="py-5">
-      <div className="container-fluid">
-        <div className="container">
-          <div className="d-flex flex-row">
-            <div className="col-4 border rounded rounded-3 me-4">
-              <div className="px-4 py-5">
-                <div className="text-center mb-5">
-                  <img src={noPhoto} alt="" className="w-50 rounded-pill" />
-                </div>
-                <div className="fw-bold h4">Louis Tomlinson</div>
-                <div className="fw-bold">Web Developer</div>
-                <div className="text-muted">London, UK</div>
-                <div className="text-muted mb-3">0812-3456-7889</div>
-                <div className="text-muted mb-4">
-                  {" "}
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </div>
-                <button className="btn w-100 btn-primary mb-4">Hire</button>
-                <div className="fw-bold h5">Skill</div>
-                <div className="text-muted">louistommo@gmail.com</div>
-                <div className="text-muted">@Louis91</div>
-                <div className="text-muted">@Louistomma</div>
-                <div className="text-muted">@Louistomma</div>
-              </div>
-            </div>
-
-            <div className="col-8 border rounded-3 ">
-              <div className="px-4 py-5">
-                <ul className="nav nav-pills" id="myTab" role="tablist">
-                  <li className="nav-item" role="presentation">
-                    <button
-                      className="nav-link active-custom"
-                      id="portofolio-tab"
-                      data-bs-toggle="tab"
-                      data-bs-target="#portofolio"
-                      type="button"
-                      role="tab"
-                      aria-controls="portofolio"
-                      aria-selected="true"
-                    >
-                      Portofolio
-                    </button>
-                  </li>
-                  <li className="nav-item" role="presentation">
-                    <button
-                      className="nav-link "
-                      id="userExperience-tab"
-                      data-bs-toggle="tab"
-                      data-bs-target="#userExperience"
-                      type="button"
-                      role="tab"
-                      aria-controls="userExperience"
-                      aria-selected="false"
-                    >
-                      Pengalaman Kerja
-                    </button>
-                  </li>
-                </ul>
-                <div className="tab-content" id="myTabContent">
-                  <div
-                    className="tab-pane fade show active mt-3"
-                    id="portofolio"
-                    role="tabpanel"
-                    aria-labelledby="portofolio-tab"
-                  >
-                    <ListPortofolio />
+    <div>
+      {/* Header */}
+      <Navbar />
+      <div className="background-purple-white">
+        <div className="py-5">
+          <div className="container-fluid ">
+            <div className="container">
+              <div className="d-flex flex-row align-items-start">
+                <div className="col-4 border rounded rounded-3 me-4 bg-white">
+                  <div className="px-4 py-5">
+                    <div className="text-center mb-5">
+                      <img src={noPhoto} alt="" className="w-50 rounded-pill" />
+                    </div>
+                    <div className="fw-bold h4">Louis Tomlinson</div>
+                    <div className="fw-bold">Web Developer</div>
+                    <div className="text-muted">London, UK</div>
+                    <div className="text-muted mb-3">0812-3456-7889</div>
+                    <div className="text-muted mb-4">
+                      {" "}
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </div>
+                    <button className="btn w-100 btn-primary mb-4">Hire</button>
+                    <div className="fw-bold h5">Skill</div>
+                    <div className="text-muted">louistommo@gmail.com</div>
+                    <div className="text-muted">@Louis91</div>
+                    <div className="text-muted">@Louistomma</div>
+                    <div className="text-muted">@Louistomma</div>
                   </div>
-                  <div
-                    className="tab-pane fade mt-3"
-                    id="userExperience"
-                    role="tabpanel"
-                    aria-labelledby="userExperience-tab"
-                  >
-                    <ListPengalaman />
+                </div>
+
+                <div className="container border rounded-3 bg-white">
+                  <div className="px-4 py-5">
+                    <ul className="nav nav-pills" id="myTab" role="tablist">
+                      <li className="nav-item" role="presentation">
+                        <button
+                          className="nav-link active-custom"
+                          id="portofolio-tab"
+                          data-bs-toggle="tab"
+                          data-bs-target="#portofolio"
+                          type="button"
+                          role="tab"
+                          aria-controls="portofolio"
+                          aria-selected="true"
+                        >
+                          Portofolio
+                        </button>
+                      </li>
+                      <li className="nav-item" role="presentation">
+                        <button
+                          className="nav-link "
+                          id="userExperience-tab"
+                          data-bs-toggle="tab"
+                          data-bs-target="#userExperience"
+                          type="button"
+                          role="tab"
+                          aria-controls="userExperience"
+                          aria-selected="false"
+                        >
+                          Pengalaman Kerja
+                        </button>
+                      </li>
+                    </ul>
+                    <div className="tab-content" id="myTabContent">
+                      <div
+                        className="tab-pane fade show active mt-3"
+                        id="portofolio"
+                        role="tabpanel"
+                        aria-labelledby="portofolio-tab"
+                      >
+                        <ListPortofolio />
+                      </div>
+                      <div
+                        className="tab-pane fade mt-3"
+                        id="userExperience"
+                        role="tabpanel"
+                        aria-labelledby="userExperience-tab"
+                      >
+                        <ListPengalaman />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -93,6 +102,9 @@ export default function UserProfile() {
           </div>
         </div>
       </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
