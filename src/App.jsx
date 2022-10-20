2ac3120cc17ce2cc7826ecaae959f723b1afb54b
KevinReyhanW <kevinreyhanww@gmail.com>
2022-10-20 14:24:26 +0700

    update:signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
 import HomePage from "./pages/HomePage";
 import UserProfile from "./pages/UserProfile";
 import LandingPage from "./pages/LandingPage";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/company-profile" element={<CompanyProfile />} />
         <Route path="/user-hire" element={<UserHire />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
